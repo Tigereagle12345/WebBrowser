6e6a552ccd00a36ffe61d9fd351713206e46f413
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-19 09:22:29 -0400

    Update main.py

--- WebCrawler/main.py
+++ WebCrawler/main.py
@@ -1,1 +1,20 @@
+import selenium
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 
+# Selenium Setup
+driver = webdriver.Chrome(ChromeDriverManager().install())
+chrome_url = "https://www.google.com/"
+driver.get(chrome_url)
+
+# Functions
+def robots():
+  url = driver.current_url
+  robots_url = str(url + "robots.txt")
+  
+  driver.get(robots_url)
+  source = driver.page_source
+  if "User-agent:" in source:
+    page_text = driver.find_element_by_xpath("/html/body").text
+    print(page_text)
+  
