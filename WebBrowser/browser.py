a278c24f7a6b264fa9c974e881f0dd222e86f5a9
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-19 10:34:53 -0400

    Update browser.py

--- WebBrowser/browser.py
+++ WebBrowser/browser.py
@@ -1,14 +1,9 @@
 import os
 import webbrowser
 import sys
-
 os.system("cd .. /")
-
 sys.path.insert(1, "/WebCrawler/")
-
 from database import *
 
-url = input
-
-page = 'file:///'+os.getcwd()+'/' + url + ".html"
+page = "file:///"+os.getcwd()+"/" + url + ".html"
 webbrowser.open_new_tab(filename)
