09a6ee275c29d356a291d48698179c2033953714
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-19 10:33:45 -0400

    Create browser.py

---
+++ WebBrowser/browser.py
@@ -0,0 +1,14 @@
+import os
+import webbrowser
+import sys
+
+os.system("cd .. /")
+
+sys.path.insert(1, "/WebCrawler/")
+
+from database import *
+
+url = input
+
+page = 'file:///'+os.getcwd()+'/' + url + ".html"
+webbrowser.open_new_tab(filename)
