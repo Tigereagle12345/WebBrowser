c9cc820e290be3b57dde2492d67207e50de224eb
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-19 09:39:21 -0400

    Update main.py

--- WebCrawler/main.py
+++ WebCrawler/main.py
@@ -1,6 +1,7 @@
 import selenium
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from ./Wordlist/wordlist import words
 
 # Selenium Setup
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -17,4 +18,8 @@ def robots():
   if "User-agent:" in source:
     page_text = driver.find_element_by_xpath("/html/body").text
     print(page_text)
+ 
+def storedata(word):
+  url = driver.current_url
+  source = driver.page_source
   
