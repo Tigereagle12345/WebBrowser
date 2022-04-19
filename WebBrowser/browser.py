e98a67b2e0b33b77bead2cde564f1326ca6751ea
Tigereagle12345 <97179347+Tigereagle12345@users.noreply.github.com>
2022-04-19 11:12:00 -0400

    Update browser.py

--- WebBrowser/browser.py
+++ WebBrowser/browser.py
@@ -5,5 +5,19 @@ os.system("cd .. /")
 sys.path.insert(1, "/WebCrawler/")
 from database import *
 
+search = input("Search Browser: ")
+ori_search = search
+
+# Remove characters
+search = search.replace("the", "")
+search = search.replace("", "-")
+
+# Split strings into words
+search = search.split("-")
+
+print("Results for " + str(ori_search))
+for word in search:
+  print
+
 page = "file:///"+os.getcwd()+"/" + url + ".html"
 webbrowser.open_new_tab(filename)
